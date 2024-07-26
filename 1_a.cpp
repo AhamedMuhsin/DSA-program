b1c5953b8cbf77f09a1ee98ed85545b69ec6d603
Ahamed Muhsin <muhsinjham@gmail.com>
2024-07-26 20:16:26 +0530

    added new function

--- 1_a.cpp
+++ 1_a.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//for taking input from users in array
 void inputArray(int arr[], int &n) {
     cout << "Enter the number of elements: ";
     cin >> n;
@@ -10,6 +11,7 @@ void inputArray(int arr[], int &n) {
     }
 }
 
+//for display the element of array
 void displayArray(int arr[], int n) {
 	cout<<"The Stored values in array is : ";
     for (int i = 0; i < n; i++) {
@@ -18,7 +20,8 @@ void displayArray(int arr[], int n) {
     cout << endl;
 }
 
-void searchElement(int arr[], int n){
+//linear search for searching element
+void linearSearch(int arr[], int n){
 	int element;
 	cout<<"Enter the element you want to search : ";
 	cin>>element;
@@ -35,6 +38,35 @@ void searchElement(int arr[], int n){
 	}
 }
 
+//binary search for searching a element
+void binarySearch(int arr[],int n){
+	int element;
+	cout<<"Enter the element you want to search : ";
+	cin>>element;
+	bool found = false;
+	int lb,ub,mid;
+	lb = 0;
+	ub = n-1;
+	while(lb<=ub){
+		mid=(lb+ub)/2;
+		if(arr[mid]==element){
+			cout<<"The Element was found at "<<mid<<endl;
+			found = true;
+			break;
+		}
+		if(arr[mid]<element){
+			lb=mid+1;
+		}
+		else{
+			ub=mid-1;
+		}
+	}
+	if(!found){
+		cout<<"The Element was not found."<<endl;
+	}
+}
+
+//sorting function to sort the element of array
 void sortArray(int arr[], int n) {
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -48,6 +80,7 @@ void sortArray(int arr[], int n) {
     cout << "Array Sorted." << endl;
 }
 
+//reverse function to reverse the element of array
 void reverseArray(int arr[], int n) {
     for (int i = 0; i < n / 2; i++) {
         int temp = arr[i];
@@ -57,6 +90,7 @@ void reverseArray(int arr[], int n) {
     cout << "Array Reversed." << endl;
 }
 
+//insertion function to insert element between,start or end of the array
 int indInsertion(int arr[],int n,int capacity){
 	int element,index;
 	cout<<"Enter the index were you want to insert : ";
@@ -75,6 +109,7 @@ int indInsertion(int arr[],int n,int capacity){
 	return 1;
 }
 
+//deletion function to delete element between,start or end of the array
 void indDeletion(int arr[],int n){
 	int index;
 	cout<<"Enter the index which you want to delete : ";
@@ -85,20 +120,24 @@ void indDeletion(int arr[],int n){
 	cout<<"Deletion done Successfully\n";
 }
 
+//starting of main program
 int main() {
     int arr[100],n = 0,choice;
     do {
+    	//using do-while loop for making the program menu driven
         cout << "\nMenu:\n";
         cout << "1. Input array\n";
         cout << "2. Display array\n";
-        cout << "3. Search element\n";
-        cout << "4. Sort array\n";
-        cout << "5. Reverse array\n";
+        cout << "3. linearSearch to found element\n";
+        cout << "4. Sort an array\n";
+        cout << "5. BinarySearch to found element\n";
         cout << "6. Insertion in array\n";
         cout << "7. Deletion in array\n";
-        cout << "8. Exit\n";
+        cout << "8. Reverse an array\n";
+        cout << "9. Exit\n";
         cout << "Enter your choice: ";
         cin >> choice;
+        //switch statement to run the function according to user choice
         switch(choice) {
             case 1:
                 inputArray(arr, n);
@@ -107,15 +146,15 @@ int main() {
                 displayArray(arr, n);
                 break;
             case 3:
-                searchElement(arr, n);
+                linearSearch(arr, n);
                 break;
             case 4:
                 sortArray(arr, n);
                 displayArray(arr, n);
                 break;
             case 5:
-                reverseArray(arr, n);
-                displayArray(arr, n);
+            	sortArray(arr,n);
+                binarySearch(arr, n);
                 break;
             case 6:
             	indInsertion(arr,n,100);
@@ -128,12 +167,16 @@ int main() {
             	displayArray(arr,n);
             	break;
             case 8:
+                reverseArray(arr, n);
+                displayArray(arr, n);
+                break;
+            case 9:
                 cout << "Exiting program." << endl;
                 break;
             default:
                 cout << "Invalid choice. Please try again." << endl;
         }
-    } while (choice != 8);
+    } while (choice != 9);
     return 0;
 }
 
